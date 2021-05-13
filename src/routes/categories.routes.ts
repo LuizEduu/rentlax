b1123e731cd8189c2d0b64a2a85508f6275bf42d
Luiz Eduardo <l.eduardo8316@gmail.com>
2021-05-13 16:33:12 -0300

    feat: validate to create a duplicate category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoryRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
+  const categoryAlreadyExists = categoryRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists" });
+  }
+
   const category = categoryRepository.create({ name, description });
 
   return response.status(201).json(category);
