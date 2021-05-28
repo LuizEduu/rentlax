14e47df135a5943e75ab7d69894c7249e83989b3
= <=>
2021-05-28 19:39:51 -0300

    refactor: add dependencyInjection reponsability to tsyringe

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { User } from "../../entities/user";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
   async handle(
     request: Request,
     response: Response
   ): Promise<Response<User | Error>> {
+    const createUserUseCase = container.resolve(CreateUserUseCase);
     const { name, username, password, email, admin, driver_license } =
       request.body;
-    const user = await this.createUserUseCase.execute({
+    const user = await createUserUseCase.execute({
       name,
       username,
       password,
