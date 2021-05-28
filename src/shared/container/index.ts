d028916f3cef10617c7765ff05bdc52d70ee28f7
= <=>
2021-05-28 19:37:04 -0300

    feat: add usersRepositoryDependency to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { UsersRepositoryImpl } from "../../modules/accounts/repositories/implementations/UsersRepositoryImpl";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepositoryImpl } from "../../modules/cars/repositories/implementations/CategoriesRepositoryImpl";
 import { SpecificationsRepositoryImpl } from "../../modules/cars/repositories/implementations/SpecificationsRepositoryImpl";
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepositoryImpl",
   SpecificationsRepositoryImpl
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepositoryImpl",
+  UsersRepositoryImpl
 );
