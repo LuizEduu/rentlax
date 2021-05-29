de6d4eb1e69d41b62c3c8ef6b78f8523f738e995
= <=>
2021-05-29 11:12:53 -0300

    refactor: remove username property

--- src/modules/accounts/dtos/ICreateUserDTO.ts
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -1,10 +1,8 @@
 interface ICreateUserDTO {
   name: string;
-  username: string;
   password: string;
   email: string;
   driver_license: string;
-  admin?: boolean;
 }
 
 export { ICreateUserDTO };
