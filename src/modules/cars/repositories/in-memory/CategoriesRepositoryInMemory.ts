071c5d133d14e24c626e98d1a1380aadabe68c49
= <=>
2021-06-03 13:03:26 -0300

    feat: add category repository in memory

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,33 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CreateCategoryRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+  async create({ name, description }: ICreateCategoryDTO): Promise<Category> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.categories.push(category);
+
+    return category;
+  }
+}
+
+export { CreateCategoryRepositoryInMemory };
