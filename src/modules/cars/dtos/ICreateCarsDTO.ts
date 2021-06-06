0c0f2b7d06b968d8c66d48b98f148660cdba5920
= <=>
2021-06-06 16:21:27 -0300

    feat: add createCar DTO

---
+++ src/modules/cars/dtos/ICreateCarsDTO.ts
@@ -0,0 +1,13 @@
+interface ICreateCarsDTO {
+  id?: string;
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+  created_at?: Date;
+}
+
+export { ICreateCarsDTO };
