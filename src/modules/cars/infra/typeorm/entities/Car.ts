02f65a95974e4d05638fd1a49d859198650e5999
= <=>
2021-06-06 16:20:53 -0300

    feat: add car entitie

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,43 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("cars")
+class Car {
+  @PrimaryColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  daily_rate: number;
+
+  @Column()
+  available: boolean;
+
+  @Column()
+  license_plate: string;
+
+  @Column()
+  fine_amount: number;
+
+  @Column()
+  brand: string;
+
+  @Column()
+  category_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Car };
