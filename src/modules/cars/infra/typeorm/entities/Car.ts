f91949fc14fdeec41d77d015cb0d73805c6f3c6a
= <=>
2021-06-07 15:12:26 -0300

    refactor: add relation manytoone cars and categories

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,5 +1,14 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "./Category";
 
 @Entity("cars")
 class Car {
@@ -27,6 +36,10 @@ class Car {
   @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
   @Column()
   category_id: string;
 
@@ -36,6 +49,7 @@ class Car {
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+      this.available = true;
     }
   }
 }
