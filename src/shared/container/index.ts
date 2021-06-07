f5101ebdc1c2c4adfdcb4b2608948780dacfbcaa
= <=>
2021-06-07 15:21:26 -0300

    feat: add injection map to tsyringe

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from "tsyringe";
 
 import { UsersRepositoryImpl } from "@modules/accounts/infra/typeorm/repositories/UsersRepositoryImpl";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepositoryImpl } from "@modules/cars/infra/typeorm/repositories/CategoriesRepositoryImpl"
+import { CarsRepositoryImpl } from "@modules/cars/infra/typeorm/repositories/CarsRepositoryImpl";
+import { CategoriesRepositoryImpl } from "@modules/cars/infra/typeorm/repositories/CategoriesRepositoryImpl";
 import { SpecificationsRepositoryImpl } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepositoryImpl";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
@@ -20,4 +22,9 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
   "UsersRepositoryImpl",
   UsersRepositoryImpl
+);
+
+container.registerSingleton<ICarsRepository>(
+  "CarsRepositoryImpl",
+  CarsRepositoryImpl
 );
