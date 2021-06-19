61404df710b6030a044614a32f6a9b85e817329d
Luiz Eduardo <l.eduardo8316@gmail.com>
2021-06-19 18:23:10 -0300

    feat: add id to properties

--- src/modules/cars/dtos/ICreateCarsDTO.ts
+++ src/modules/cars/dtos/ICreateCarsDTO.ts
@@ -1,6 +1,7 @@
 import { Specification } from "../infra/typeorm/entities/Specification";
 
 interface ICreateCarsDTO {
+  id?: string;
   name: string;
   description: string;
   daily_rate: number;
